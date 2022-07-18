2a7641938ce60d80fc43092b864e1dad5ee49b9f
DieumegardGregory <dieumegard.gregory@gmail.com>
2022-07-18 10:32:42 +0200

    ajustements css responsive

--- src/2.Public/Header/Header.jsx
+++ src/2.Public/Header/Header.jsx
@@ -6,12 +6,12 @@ import './Header.scss';
 const Header = () => {
   return (
     <>
-    <div className="header-container">
+    <div className="header-container grid-20-80">
       <NavLink to="/">
           <img className="logo" src={logo} alt="logo-hackacook" />
         </NavLink>
         <div className="title-container">
-        <h1>Hackacook !</h1>
+          <h1>Hackacook !</h1>
         </div>
     </div>
     <div className="header-cale"></div>
